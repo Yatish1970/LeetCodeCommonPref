401480a5b5a3b0b32a527a6cab2f37535526ea2e
Yatish <yatish1970@gmail.com>
2022-12-20 23:59:16 -0500

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] strs = { "carrier", "caro", "carry" };
+            Console.WriteLine(LongestCommonPrefix(strs));
+            Console.ReadLine();
+    }
+        public static string LongestCommonPrefix(string[] strs)
+        {
+            string pref = strs[0];            
+            for(int i=1;i<strs.Length;i++)
+            {
+                while (strs[i].IndexOf(pref)!=0)
+                {
+                    if(strs[i].IndexOf(pref)!=0)
+                    {
+                        if (pref.Length != 1)
+                            pref = pref.Substring(0,pref.Length-1);
+                        else
+                            pref ="";
+                    }
+                }
+            }
+            return pref;
+        }
+    }
+}
